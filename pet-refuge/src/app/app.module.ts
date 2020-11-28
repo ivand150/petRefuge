c0ca94e4b590197c224a9dc8445778f3aa409440
Ivan Dobry <ivandobry@hotmail.es>
2020-11-28 18:59:11 +0100

    server settings and create pet detail fodler

--- pet-refuge/src/app/app.module.ts
+++ pet-refuge/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetDetailComponent } from './pet-detail/pet-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetDetailComponent
   ],
   imports: [
     BrowserModule,
