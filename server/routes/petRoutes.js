8233ca4d2d96cda76a8415d23392f1357306ce15
Ivan Dobry <ivandobry@hotmail.es>
2020-12-02 09:01:37 +0100

    pet controller

--- server/routes/petRoutes.js
+++ server/routes/petRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const petsController = require('../controllers/petsController');
+const petController = require('../controllers/petController');
 
 function routes(pets) {
   const petRouter = express.Router();
   const pet = petsController(pets);
+  const detailPet = petController(pets);
 
   petRouter.route('/')
     .get(pet.getMethod)
     .put(pet.putMethod);
+
+  petRouter.route('/detail')
+    .get(detailPet.getMethod);
   return petRouter;
 }
 
