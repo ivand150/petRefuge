738ae9e08fe53262a69ae77fd0a4cddb7a390851
Ivan Dobry <ivandobry@hotmail.es>
2020-12-05 13:08:14 +0100

    main desktop and testing

--- pet-refuge/src/app/main/main.component.spec.ts
+++ pet-refuge/src/app/main/main.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterModule } from '@angular/router';
+import { PetListComponent } from '../pet-list/pet-list.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MainComponent } from './main.component';
 
@@ -8,6 +12,7 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterModule.forRoot([]), RouterTestingModule.withRoutes([{ path: 'list', component: PetListComponent }])],
       declarations: [MainComponent]
     })
       .compileComponents();
@@ -21,5 +26,23 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call get findPet on click find button', async (done) => {
+    const spyFn = spyOn(component, 'findPet').and.callThrough();
+    const compiled = fixture.nativeElement;
+    const mainButton = compiled.querySelector('.first__button');
+    mainButton.click();
+    expect(spyFn).toHaveBeenCalled();
+    done();
+  });
+
+  it('should call get findPets on click find button', async (done) => {
+    const spyFn = spyOn(component, 'findPets').and.callThrough();
+    const compiled = fixture.nativeElement;
+    const mainButton = compiled.querySelector('.second__button');
+    mainButton.click();
+    expect(spyFn).toHaveBeenCalled();
+    done();
   });
 });
