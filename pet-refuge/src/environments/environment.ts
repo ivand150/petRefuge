6280f2ee38c9540a8244562a3a164f0a1338c461
Ivan Dobry <ivandobry@hotmail.es>
2020-12-06 13:33:04 +0100

    added firebase login

--- pet-refuge/src/environments/environment.ts
+++ pet-refuge/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCaDe9tsZoDIJzyoD0-C1hr4n6Nv-2Rhtw',
+    authDomain: 'petrefuge-8877a.firebaseapp.com',
+    projectId: 'petrefuge-8877a',
+    storageBucket: 'petrefuge-8877a.appspot.com',
+    messagingSenderId: '309073038928',
+    appId: '1:309073038928:web:7eb1341da93d2c4791cb55',
+    measurementId: 'G-5ELR40KQ8Y'
+  }
 };
 
 /*
