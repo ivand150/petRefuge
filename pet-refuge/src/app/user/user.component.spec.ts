784114c186695a9ee5e640faa05c7036b0ae37aa
Ivan Dobry <ivandobry@hotmail.es>
2020-12-07 20:27:32 +0100

    failed attempt

--- pet-refuge/src/app/user/user.component.spec.ts
+++ pet-refuge/src/app/user/user.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterModule } from '@angular/router';
 
 import { UserComponent } from './user.component';
 
@@ -8,6 +10,7 @@ describe('UserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterModule.forRoot([])],
       declarations: [UserComponent]
     })
       .compileComponents();
